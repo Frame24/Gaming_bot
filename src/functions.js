93b33dc04dbc51cdc1f93175e1c039ad015d8a79
tabletka2401@gmail.com <tabletka2401@gmail.com>
2023-09-15 14:45:17 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,7 @@
 function getRandomInt(max) {
-return Math.round(Math.random() * max);
+    coins_given = Math.round(Math.random() * max);
+    coins_count += coins_given
+    return coins_given;
 }
+
+var coins_count = 0
